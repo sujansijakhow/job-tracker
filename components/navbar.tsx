2a323842c81b60d30eaf45a3e9339de60fbe7b5c
sujansijakhow <sujansijakhow@gmail.com>
2026-08-22 09:26:06 +0545

    implement nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,11 @@
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
     <nav className="border-b border-gray-200 bg-white">
-      <div className="container mx-auto flex h-16 items-center p-4">
+      <div className="container mx-auto flex h-16 items-center p-4 justify-between">
         <Link
           href={"/"}
           className="flex items-center gap-2 text-xl font-semibold text-primary"
@@ -12,6 +13,19 @@ const Navbar = () => {
           <Briefcase />
           Job Tracker
         </Link>
+
+        <div className="flex items-center gap-4">
+          <Link href={"/sign-in"}>
+            <Button variant="ghost" className="text-gray-700 hover:text-black">
+              Log In
+            </Button>
+          </Link>
+          <Link href={"/sign-up"}>
+            <Button className="bg-primary hover:bg-primary/90">
+              Start for free
+            </Button>
+          </Link>
+        </div>
       </div>
     </nav>
   );
